8856624a9db2957abcb248bb1d261a8d8f1c1f84
SERN_INTERN <SERN_INTERN@SERN-WP-HS00GDY.SERN.MIL>
2023-07-07 09:56:10 -0400

    Add existing files

---
+++ grocerylist2.py
@@ -0,0 +1,94 @@
+import tkinter
+import tkinter.messagebox
+import pickle
+
+root= tkinter.Tk()
+root.title("Grocery list")
+
+def add_task():
+    task = entry_task.get()
+    if task != "":
+        listbox_task.insert(tkinter.END, task)
+        entry_task.delete(0, tkinter.END)
+    else:
+        tkinter.messagebox.showwarning(title= "Warning!", message="You must enter a task")
+
+def delete_task():
+    try:
+        task_index = listbox_task.curselection()[0]
+        listbox_task.delete(task_index)
+    except:
+         tkinter.messagebox.showwarning(title= "Warning!", message="You must select a task")
+
+def load_tasks():
+    tasks= pickle.load(open("tasks.dat","rb"))
+
+def save_tasks():
+    tasks = listbox_task.get(0, listbox_tasks.size())
+    listbox_task.delete(0, tkinter.END)
+    pickle.dump(tasks, open("tasks.dat", "wb"))
+    
+def load_list
+    class main:
+        def _init_(self,item,price)
+            self.item = item
+            self.pruce = price
+        with open("item cost.txt","r") as item_cost:
+            lines = item_cost.readlines()
+            items = []
+            
+        for 1 in lines:
+            as_list = 1.split(",")
+            cow = main(as_list[0],as_list[1].replace("\n", ""))
+            items.append(cow)
+            
+        for items in items:
+            listbox_task.insert(tkinter.END, items.item)
+            listbox_tasks1.insert(tkinter.END, items.price)
+            
+        def mutiple_yview(*args):
+            listbox_tasks .yview(*args)
+            listbox_tasks1 .yview(*args)
+            listbox_tasks2 .yview(*args)
+
+
+# Create GUI
+frame_task = tkinter.Frame(root)
+frame_task.pack()
+listbox_task = tkinter.Listbox(frame_task, height=10, width=50)
+listbox_task.pack(side=tkinter.LEFT)
+
+listbox_task = tkinter.Listbox(frame_task, height=10, width=50)
+listbox_task.pack(side=tkinter.LEFT)
+
+scrollbar_task = tkinter.Scrollbar(frame_task)
+scrollbar_task.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+
+scrollbar_task.config(command=listbox_task.yview)
+
+item_label=tkinter.Label(text= "Price")
+item_label.pack()
+entry_task = tkinter.Entry(root, width=50)
+entry_task.pack()
+
+item_label=tkinter.Label(text= "Item")
+item_label.pack()
+entry_task = tkinter.Entry(root, width=50)
+entry_task.pack()
+
+
+button_add_task = tkinter.Button(root, text= "add task", width=48, command=add_task)
+button_add_task.pack()
+
+button_add_task = tkinter.Button(root, text= "delete task", width=48, command=add_task)
+button_add_task.pack()
+
+button_add_task = tkinter.Button(root, text= "load task", width=48, command=add_task)
+button_add_task.pack()
+
+button_add_task = tkinter.Button(root, text= "save task", width=48, command=add_task)
+button_add_task.pack()
+
+root.mainloop()
+
